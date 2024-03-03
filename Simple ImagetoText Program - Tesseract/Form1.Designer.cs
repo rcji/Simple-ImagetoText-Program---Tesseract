8f754d8e5fbf5278fcc32f4514a3a75d8e8bc7bc
RCryeji <raphaelchase302@gmail.com>
2024-03-03 19:19:28 +0800

    Add project files.

---
+++ Simple ImagetoText Program - Tesseract/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Simple_ImagetoText_Program___Tesseract
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.selectImageButton = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.copyTextButton = new System.Windows.Forms.Button();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fileNameTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(449, 12);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(546, 447);
+            this.textBox.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // selectImageButton
+            // 
+            this.selectImageButton.Location = new System.Drawing.Point(167, 397);
+            this.selectImageButton.Name = "selectImageButton";
+            this.selectImageButton.Size = new System.Drawing.Size(263, 62);
+            this.selectImageButton.TabIndex = 2;
+            this.selectImageButton.Text = "Select Image";
+            this.selectImageButton.UseVisualStyleBackColor = true;
+            this.selectImageButton.Click += new System.EventHandler(this.selectImageButton_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox.Image")));
+            this.pictureBox.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox.InitialImage")));
+            this.pictureBox.Location = new System.Drawing.Point(12, 30);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(418, 321);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 3;
+            this.pictureBox.TabStop = false;
+            // 
+            // copyTextButton
+            // 
+            this.copyTextButton.Location = new System.Drawing.Point(12, 397);
+            this.copyTextButton.Name = "copyTextButton";
+            this.copyTextButton.Size = new System.Drawing.Size(149, 62);
+            this.copyTextButton.TabIndex = 4;
+            this.copyTextButton.Text = "Copy Text To Clipboard";
+            this.copyTextButton.UseVisualStyleBackColor = true;
+            this.copyTextButton.Click += new System.EventHandler(this.copyTextButton_Click);
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Enabled = false;
+            this.addressTextBox.Location = new System.Drawing.Point(12, 369);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(418, 22);
+            this.addressTextBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 347);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Address:";
+            // 
+            // fileNameTextBox
+            // 
+            this.fileNameTextBox.Enabled = false;
+            this.fileNameTextBox.Location = new System.Drawing.Point(84, 2);
+            this.fileNameTextBox.Name = "fileNameTextBox";
+            this.fileNameTextBox.Size = new System.Drawing.Size(346, 22);
+            this.fileNameTextBox.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "FileName:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1007, 471);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.fileNameTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.addressTextBox);
+            this.Controls.Add(this.copyTextButton);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.selectImageButton);
+            this.Controls.Add(this.textBox);
+            this.Name = "Form1";
+            this.Text = "Text from Image";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button selectImageButton;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button copyTextButton;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox fileNameTextBox;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Simple ImagetoText Program - Tesseract/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tesseract;
+using Tesseract.Interop; 
+
+namespace Simple_ImagetoText_Program___Tesseract
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        
+        private void selectImageButton_Click(object sender, EventArgs e)
+        {
+            
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;*.gif";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox.Image = new Bitmap(ofd.FileName);
+            }
+
+            // make sure to change the path to the tessdata folder to your own path
+            //you need to download the tessdata folder from the tesseract github page "https://github.com/tesseract-ocr/tessdata"
+            using (var engine = new TesseractEngine(@"C:\Users\Predator\Source\Repos\tessdata", "eng", EngineMode.Default))
+            {
+                using (var img = Pix.LoadFromFile(ofd.FileName))
+                {
+                    using (var page = engine.Process(img))
+                    {
+                        
+                        var text = page.GetText();
+                        
+                        textBox.Text = text;
+                    }
+                }
+            }
+            addressTextBox.Text = ofd.FileName;
+            fileNameTextBox.Text = ofd.SafeFileName;
+            copyTextButton.Text = "Copy Text to Clipboard";
+        }
+
+        private void copyTextButton_Click(object sender, EventArgs e)
+        {
+            copyTextButton.Text = "Copied!";
+            Clipboard.SetText(textBox.Text);
+        }
+    }
+
+        
+    }
+
